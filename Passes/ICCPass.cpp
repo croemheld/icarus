670d3a66dd493e153c12307423d3c3bdb77cfeb2
Christopher Roemheld <roemheld@protonmail.com>
2023-03-30 00:29:54 +0200

    Manually count indirect calls in LLVM < 8

--- Passes/ICCPass.cpp
+++ Passes/ICCPass.cpp
@@ -2,7 +2,11 @@
 // Created by croemheld on 26.12.2022.
 //
 
+#include <icarus/Support/Clang.h>
+
+#if ICARUS_CLANG_VERSION >= 8
 #include <llvm/Analysis/IndirectCallVisitor.h>
+#endif
 
 #include <icarus/Passes/ICCPass.h>
 
@@ -22,7 +26,18 @@ int ICCPass::runAnalysisPass(PassArguments &IPA) {
   forEachModule(IPA, [&](IcarusModule &IM) {
     llvm::Module *M = IM.getModule();
     for (llvm::Function &F : *M) {
+#if ICARUS_CLANG_VERSION >= 8
       IndirectCalls[IM.getFilePath()] += llvm::findIndirectCalls(F).size();
+#else
+      for (llvm::BasicBlock &BB : F) {
+        for (llvm::Instruction &I : BB) {
+          if (llvm::CallInst *C = llvm::dyn_cast<llvm::CallInst>(&I)) {
+            if (!C->getCalledFunction())
+              IndirectCalls[IM.getFilePath()]++;
+          }
+        }
+      }
+#endif
     }
   });
 
