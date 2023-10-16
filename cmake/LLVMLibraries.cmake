08fa7358c97629dff159ea71fba44e532e6b45a5
Christopher Roemheld <roemheld@protonmail.com>
2023-10-16 19:24:39 +0200

    Restore LLVMLibraries.cmake without LLVMRemarks

--- cmake/LLVMLibraries.cmake
+++ cmake/LLVMLibraries.cmake
@@ -3,13 +3,20 @@ set(LLVM_LIBRARIES
         LLVMIRReader
         LLVMAsmParser
         LLVMBitReader
+        LLVMCore
         LLVMSupport
         LLVMDemangle
         CACHE INTERNAL ""
 )
 
-# Conclude LLVM libraries with LLVMCore
-list(APPEND LLVM_LIBRARIES LLVMCore)
+# Further libraries to link against for LLVM versions >= 9.
+# The LLVM_PACKAGE_VERSION variable is usually set after the LLVM package was located
+# via the find_package command. However, if the CMake file is executed in script mode
+# the aforementioned command does not work. To circumvent this issue, we are required
+# to set the variable manually via -DLLVM_PACKAGE_VERSION.
+if (LLVM_PACKAGE_VERSION GREATER_EQUAL 9)
+    list(APPEND LLVM_LIBRARIES LLVMBitstreamReader)
+endif (LLVM_PACKAGE_VERSION GREATER_EQUAL 9)
 
 # We can call this script from the command line, necessary for docker image creation.
 if (NOT CMAKE_PARENT_LIST_FILE)
