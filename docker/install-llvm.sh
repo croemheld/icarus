63328075b7d5a0606a78cb276e2b7f57749cc67a
Christopher Roemheld <roemheld@protonmail.com>
2023-11-11 12:36:09 +0100

    Use dedicated patch function of LLVM and Clang CMake files, define targets

--- docker/install-llvm.sh
+++ docker/install-llvm.sh
@@ -2,6 +2,14 @@
 
 set -e
 
+# Some files need to be patched in order to allow for an incomplete installation of LLVM and Clang.
+# We use this function to patch these here. We also should not use sed -i as the option is not part
+# of all implementations and might lead to unrecognized or erroneous options.
+patch_cmake_include() {
+  sed "s/include(\"\${${1}}\/${2}\")/# &/" "${3}" > out.cmake
+  mv out.cmake "${3}"
+}
+
 CMAKE_ADDRESS="$(pwd)/llvm/CMakeLists.txt"
 LLVM_MAJORVER="$(grep 'LLVM_VERSION_MAJOR ' "${CMAKE_ADDRESS}" | awk -F'[()]' '{print $2}' | cut -d ' ' -f2)"
 CMAKE_TARGETS="${*}"
@@ -11,22 +19,24 @@ CMAKE_TARGETS="${*}"
 # or any builtin headers, add target! #
 #######################################
 
+CMAKE_TARGETS="${CMAKE_TARGETS} clang clang-headers llvm-headers cmake-exports"
+
 if [ "${LLVM_MAJORVER}" -gt "8" ]; then
-	CMAKE_TARGETS="${CMAKE_TARGETS} install-clang-resource-headers"
+	CMAKE_TARGETS="${CMAKE_TARGETS} libclang libclang-headers clang-resource-headers clang-cmake-exports clangTooling"
 fi
 
 #######################################
 # Only install minimum LLVM libraries #
 #######################################
 
-BUILD_TARGETS=$(cmake -DLLVM_PACKAGE_VERSION="${LLVM_MAJORVER}" -P ../LLVMLibraries.cmake 2>&1 | xargs -d ';')
-CMAKE_TARGETS="${CMAKE_TARGETS} $(printf '%s' "${BUILD_TARGETS}" | sed 's/[^ ]* */install-&/g')"
+LIBRARY_NAMES=$(cmake -DLLVM_PACKAGE_VERSION="${LLVM_MAJORVER}" -P ../LLVMLibraries.cmake 2>&1 | xargs -d ';')
+CMAKE_INSTALL="$(printf '%s' "${CMAKE_TARGETS} ${LIBRARY_NAMES}" | sed 's/[^ ]* */install-&/g')"
 
 #######################################
 # Build projects with CMake arguments #
 #######################################
 
-cd build && echo "Install targets: ${CMAKE_TARGETS}" && ninja ${CMAKE_TARGETS}
+cd build && echo "Install targets: ${CMAKE_INSTALL}" && ninja ${CMAKE_INSTALL}
 
 #######################################
 # Comment include of LLVMExports file #
@@ -35,9 +45,7 @@ cd build && echo "Install targets: ${CMAKE_TARGETS}" && ninja ${CMAKE_TARGETS}
 # We comment the include of the LLVMExports.cmake file in order to generate a docker image with the
 # bare minimum of libraries that are needed for this project. If we include that file, we are going
 # to get a CMake error saying that the LLVM libraries were not installed completely.
-
-# Do not use sed -i as this is not always supported or handled correctly. We also ignore spellcheck
-# complaining about expressions not expanding in single quotes as the ${LLVM_CMAKE_DIR} variable is
-# used like that in the LLVMConfig.cmake file.
-sed 's/include(\"${LLVM_CMAKE_DIR}\/LLVMExports.cmake\")/# &/' /usr/local/lib/cmake/llvm/LLVMConfig.cmake > out.cmake
-mv out.cmake /usr/local/lib/cmake/llvm/LLVMConfig.cmake
+patch_cmake_include "LLVM_CMAKE_DIR" "LLVMExports.cmake" "/usr/local/lib/cmake/llvm/LLVMConfig.cmake"
+if [ "${LLVM_MAJORVER}" -gt "8" ]; then
+  patch_cmake_include "CLANG_CMAKE_DIR" "ClangTargets.cmake" "/usr/local/lib/cmake/clang/ClangConfig.cmake"
+fi
